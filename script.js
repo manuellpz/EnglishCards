9bfb71bb8662e50e205453d80ad423fc19f7597e
Manuel Lopez <manuellpz@gmail.com>
2023-09-29 18:27:06 -0700

    Some changes in styles

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
-const cardPhrase = document.querySelector('.card_phrase');
-const cardContainer = document.querySelector('.card_container');
+const cardPhrase = document.querySelector(".card_phrase");
+const cardContainer = document.querySelector(".card_container");
 let actualPhrase = "";
 let words;
 let meaning;
 let counter = 0;
 
-addEventListener('load',()=>{
-  fetch('./BACKEND/operations.php?consulta')
-  .then(response => response.json())
-  .then(data => {
-   words = data;
-   actualPhrase = data[0].word
-   cardPhrase.innerHTML = actualPhrase
-  })
-  .catch(error => console.error(error))
-})
+addEventListener("load", () => {
+  fetch("./BACKEND/operations.php?consulta")
+    .then((response) => response.json())
+    .then((data) => {
+      words = data;
+      actualPhrase = data[0].word;
+      cardPhrase.innerHTML = actualPhrase;
+    })
+    .catch((error) => console.error(error));
+});
 
-cardContainer.addEventListener('click',()=>{
-  meaning = words[counter].meaning
-  cardPhrase.innerHTML = meaning
-  cardContainer.style.background = "black"
-  cardPhrase.style.color = "yellow";
-})
+cardContainer.addEventListener("click", () => {
+  meaning = words[counter].meaning;
+  cardPhrase.innerHTML = meaning;
+  cardContainer.classList.add("meaning");
+});
 
-document.querySelector('#btn_next').addEventListener('click',()=>{
+document.querySelector("#btn_next").addEventListener("click", () => {
   counter++;
-  if(counter == words.length) counter = 0;
-  actualPhrase = words[counter].word
-  cardPhrase.innerHTML = actualPhrase
-  cardContainer.style.background = "yellow"
-  cardPhrase.style.color = "black"
-})
+  if (counter == words.length) counter = 0;
+  actualPhrase = words[counter].word;
+  cardContainer.classList.remove("meaning");
+  cardPhrase.innerHTML = actualPhrase;
+});
